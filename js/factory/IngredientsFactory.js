807e0f487cee2db7751e6f334b396a3a53dc3079
leen1515 <leventredeparis@gmail.com>
2023-05-22 12:25:14 +0200

    fix() : gramme /g replace

--- js/factory/IngredientsFactory.js
+++ js/factory/IngredientsFactory.js
@@ -1,4 +1,3 @@
-import { caseFirstLetterNormalize } from './../utilsfunction.js'
 export class IngredientsFactory {
   constructor (ingredient, quantity, unit) {
     this.ingredient = ingredient
@@ -17,10 +16,10 @@ export class IngredientsFactory {
     quantity.setAttribute('class', 'quantity col-auto align-self-start')
     unit.setAttribute('class', 'unit col-auto align-self-start')
 
-    ingredient.textContent = `${caseFirstLetterNormalize(this.ingredient)}`
+    ingredient.textContent = `${this.ingredient}`
     quantity.textContent = `: ${this.quantity} `
     if (this.unit !== undefined) {
-      unit.textContent = this.unit
+      unit.textContent = this.unit.replace('grammes', 'g')
     }
 
     ingredients.appendChild(ingredient)
