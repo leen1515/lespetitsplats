8c2245e88b2f8ded70644f315e35afd19ecf0b75
leen1515 <leventredeparis@gmail.com>
2023-05-22 23:49:27 +0200

    feat(): resout le probleme des tags singuliers et pluriels

--- js/utilsfunction.js
+++ js/utilsfunction.js
@@ -4,12 +4,24 @@ export function caseFirstLetterNormalize (letter) {
   return (letterLowerCase + '').charAt(0).toUpperCase() + letterLowerCase.substr(1)
 }
 
-// retourne qu'un element des multiples elements semblables
+// retourne qu'un element des multiples elements semblables, résout le problème des singuliers et pluriels
 // utilisé pour la liste des tags
 export function doubleDatas (datas) {
+  // filtre les doublons
   const oneData = datas.filter((data, index) => {
     return datas.indexOf(data) === index
   })
-
-  return oneData
+  // initialise un nouveau tableau pour gerer les S
+  const oneDataS = []
+  oneData.forEach((data, index, parent) => {
+    // si le dernier caractère de la donnée est S & que les avant-derniers caractère ne sont pas trouvé dans la liste parent
+    if (data.charAt(data.length - 1) === 's' && parent.includes(data.substr(0, data.length - 1)) === false) {
+      // ajoute la donnée avec son S car cela veut dire, qu'elle ne se rencontre jamais en double sans le S
+      oneDataS.push(data)
+    } else if (data.charAt(data.length - 1) !== 's') {
+      // ajoute toutes les autres données sans S
+      oneDataS.push(data)
+    }
+  })
+  return oneDataS
 }
